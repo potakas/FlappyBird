38bc5a555b8fd90a55994268333bbdef0b11dcf1
potakas <panospotis@windowslive.com>
2023-05-11 13:34:14 +0300

    add continuity for the obstacles.

--- physics/physics.js
+++ physics/physics.js
@@ -1,4 +1,5 @@
 import Matter from "matter-js";
+import { getPipeSizePosPair, windowWidth } from "../utils/random";
 
 const Physics = (entities, { touches, time, dispatch }) => {
   let engine = entities.physics.engine;
@@ -15,6 +16,12 @@ const Physics = (entities, { touches, time, dispatch }) => {
 
   Matter.Engine.update(engine, time.delta);
   for (let i = 1; i <= 2; i++) {
+    if(entities[`ObstacleTop${i}`].body.bounds.max.x<=0){
+        const pipeSizePos= getPipeSizePosPair(windowWidth*0.9)
+
+        Matter.Body.setPosition(entities[`ObstacleTop${i}`].body, pipeSizePos.pipeTop.pos);
+        Matter.Body.setPosition(entities[`ObstacleBottom${i}`].body, pipeSizePos.pipeBottom.pos);
+    }
     Matter.Body.translate(entities[`ObstacleTop${i}`].body, { x: -3, y: 0 });
     Matter.Body.translate(entities[`ObstacleBottom${i}`].body, { x: -3, y: 0 });
   }
