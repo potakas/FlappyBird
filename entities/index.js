6711d82d7be016773926bcdbd9f9ebea24c52985
potakas <panospotis@windowslive.com>
2023-05-11 12:58:18 +0300

    add floor to the game

--- entities/index.js
+++ entities/index.js
@@ -1,5 +1,10 @@
 import BirdRender from "../components/Bird";
 import Matter from "matter-js";
+import FloorRender from "../components/Floor";
+import { Dimensions } from "react-native";
+
+const windowHeight= Dimensions.get("window").height;
+const windowWidth= Dimensions.get("window").width;
 
 const restart = () => {
   let engine = Matter.Engine.create({ enableSleeping: false });
@@ -15,6 +20,12 @@ const restart = () => {
       { x: 50, y: 400 },
       { height: 40, width: 40 }
     ),
+    Floor:FloorRender(
+      world,
+      "brown",
+      {x:windowWidth/2, y:windowHeight},
+      {height: 50, width: windowWidth}
+    )
   };
 };
 
