8db4574bc798d2c0e46d91e52e4d46f052e31cd9
potakas <panospotis@windowslive.com>
2023-05-11 14:05:55 +0300

    add scoring system and restart functionality

--- App.js
+++ App.js
@@ -4,18 +4,45 @@ import { StyleSheet, Text, View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 import restart from "./entities";
 import Physics from "./physics/physics";
+import { TouchableOpacity } from "react-native";
 
 export default function App() {
-  const [running, setRunning]= useState(false);
+  const [running, setRunning] = useState(false);
+  const [gameEngine, setGameEngine] = useState(null);
+  const [points, setPoints] = useState(0);
   useEffect(() => {
     setRunning(true);
-  },[])
+  }, []);
   return (
     <View style={{ flex: 1 }}>
+      <Text
+        style={{
+          textAlign: "center",
+          fontSize: 40,
+          fontWeight: "bold",
+          margin: 20,
+        }}
+      >
+        {points}
+      </Text>
       <GameEngine
-      systems={[Physics]}
+        ref={(ref) => {
+          setGameEngine(ref);
+        }}
+        systems={[Physics]}
         entities={restart()}
         running={running}
+        onEvent={(e) => {
+          switch (e.type) {
+            case "game_over":
+              setRunning(false);
+              gameEngine.stop();
+              break;
+            case "new_point":
+              setPoints(points + 1);
+              break;
+          }
+        }}
         style={{
           position: "absolute",
           top: 0,
@@ -23,8 +50,31 @@ export default function App() {
           right: 0,
           bottom: 0,
         }}
-      ></GameEngine>
-      <StatusBar style="auto" hidden={true} />
+      >
+        <StatusBar style="auto" hidden={true} />
+      </GameEngine>
+      {!running ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "black",
+              paddingHorizontal: 10,
+              paddingVertical: 10,
+            }}
+            onPress={() =>{
+              setPoints(0);
+              setRunning(true);
+              gameEngine.swap((restart()))
+            }}
+          >
+            <Text style={{ fontWeight: "bold", color: "white", fontSize: 30 }}>
+              START GAME
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
     </View>
   );
 }
--- physics/physics.js
+++ physics/physics.js
@@ -10,21 +10,41 @@ const Physics = (entities, { touches, time, dispatch }) => {
     .forEach((t) => {
       Matter.Body.setVelocity(entities.Bird.body, {
         x: 0,
-        y: -8,
+        y: -5,
       });
     });
 
   Matter.Engine.update(engine, time.delta);
   for (let i = 1; i <= 2; i++) {
-    if(entities[`ObstacleTop${i}`].body.bounds.max.x<=0){
-        const pipeSizePos= getPipeSizePosPair(windowWidth*0.9)
+    if (
+      entities[`ObstacleTop${i}`].body.bounds.max.x <= 50 &&
+      !entities[`ObstacleTop${i}`].point
+    ) {
+      entities[`ObstacleTop${i}`].point = true;
+      dispatch({ type: "new_point" });
+    }
 
-        Matter.Body.setPosition(entities[`ObstacleTop${i}`].body, pipeSizePos.pipeTop.pos);
-        Matter.Body.setPosition(entities[`ObstacleBottom${i}`].body, pipeSizePos.pipeBottom.pos);
+
+    if (entities[`ObstacleTop${i}`].body.bounds.max.x <= 0) {
+      const pipeSizePos = getPipeSizePosPair(windowWidth * 0.9);
+
+      Matter.Body.setPosition(
+        entities[`ObstacleTop${i}`].body,
+        pipeSizePos.pipeTop.pos
+      );
+      Matter.Body.setPosition(
+        entities[`ObstacleBottom${i}`].body,
+        pipeSizePos.pipeBottom.pos
+      );
+      entities[`ObstacleTop${i}`].point = false;
     }
     Matter.Body.translate(entities[`ObstacleTop${i}`].body, { x: -3, y: 0 });
     Matter.Body.translate(entities[`ObstacleBottom${i}`].body, { x: -3, y: 0 });
   }
+
+  Matter.Events.on(engine, "collisionStart", (event) => {
+    dispatch({ type: "game_over" });
+  });
 
   return entities;
 };
