4f61c7aad06741bb618aa3b9f182d07d6a689ff3
potakas <panospotis@windowslive.com>
2023-05-11 12:49:30 +0300

    add touch to jump logic

--- physics/physics.js
+++ physics/physics.js
@@ -2,6 +2,15 @@ import Matter from "matter-js"
 
 const Physics= (entities, {touches,time,dispatch})=>{
     let engine= entities.physics.engine
+
+    //touch to jump logic
+    touches.filter(t=>t.type==='press').forEach(t=>{
+        Matter.Body.setVelocity(entities.Bird.body, {
+            x:0,
+            y:-8
+        })
+    })
+
     Matter.Engine.update(engine,time.delta)
 
     return entities;
