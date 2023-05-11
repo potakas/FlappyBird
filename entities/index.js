91355a3de611822ba12bcb2b391e7b784df34a48
potakas <panospotis@windowslive.com>
2023-05-11 12:33:46 +0300

    create first entities

---
+++ entities/index.js
@@ -0,0 +1,21 @@
+import Bird from "../components/Bird";
+import Matter from "matter-js";
+
+const restart = () => {
+  let engine = Matter.Engine.create({ enableSleeping: false });
+  let world = engine.world;
+
+  world.gravity.y = 0.4;
+
+  return {
+    physics: { engine, world },
+    Bird: Bird(
+      world,
+      "green",
+      { x: 50, y: 400 },
+      { height: 40, width: 40 }
+    ),
+  };
+};
+
+export default restart
